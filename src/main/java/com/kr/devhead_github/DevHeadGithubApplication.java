9459585f8cd22bc3eef7868828ada2aacda66649
bongyoun kim <kuhj7593@gmail.com>
2022-11-21 16:33:59 +0900

    Initial commit

---
+++ src/main/java/com/kr/devhead_github/DevHeadGithubApplication.java
@@ -0,0 +1,13 @@
+package com.kr.devhead_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHeadGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevHeadGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kr/devhead_github/DevHeadGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.devhead_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHeadGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
